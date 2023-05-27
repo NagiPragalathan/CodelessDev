bb4f65f585d3f03d41fa71b645c24d73d3f55ecc
nagipragalathan <kk2069037@gmail.com>
2023-05-27 10:59:02 +0530

    templates are updated

--- TechSpeak/urls.py
+++ TechSpeak/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from base.views import home, about, contact, services
+from base.Routes.views import home, about, contact, services
 from django.conf.urls.static import static
 from TechSpeak import settings
 
